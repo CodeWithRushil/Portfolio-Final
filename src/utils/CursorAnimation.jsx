4b85956e20d8ecb88e1bd1a672c22d13b357a28c
Rushil <workwithrushil@gmail.com>
2025-09-04 00:19:43 +0530

    animations

--- src/utils/CursorAnimation.jsx
+++ src/utils/CursorAnimation.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
 export default function CustomCursor() {
-  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
   const [cursorSize, setCursorSize] = useState(32); // Default size
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const handleMouseMove = (e) => {
@@ -39,7 +39,7 @@ export default function CustomCursor() {
 
   return (
     <motion.div
-      className="fixed bg-white rounded-full pointer-events-none mix-blend-difference z-50"
+      className="dark:fixed fixed bg-white rounded-full pointer-events-none mix-blend-difference z-50"
       style={{ width: cursorSize, height: cursorSize }}
       animate={{ x: cursorPosition.x - cursorSize / 2, y: cursorPosition.y - cursorSize / 2 }}
       transition={{ type: "spring", stiffness: 200, damping: 20 }}
